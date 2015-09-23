4f7f0079a8190b81e0ef474456da995065529325
raydeurs <briand.jp35@gmail.com>
2015-09-23 10:00:42 +0200

    Rotation du menu + netoyage script menu et porte

--- ExperienceSortieCorps/Assets/AssetsPerso/Scripts/InitSceneDoors.cs
+++ ExperienceSortieCorps/Assets/AssetsPerso/Scripts/InitSceneDoors.cs
@@ -12,56 +12,58 @@ public class InitSceneDoors : MonoBehaviour {
 	// Oui : Clic gauche souris ou touche "O"
 	// Non : Clic droit souris ou touche "N"
 
-	public GameObject piece;
-	public GameObject text;
+	[SerializeField]
+	private GameObject _piece;
+	[SerializeField]
+	private GameObject _text;
 
 	// Gestion de la largeur des portes
-	int scalesNumber=30;
-	float scalesMin=0.3f;
-	float scalesMax=1.2f;
-	List<float> scales = new List<float>();
-	int doorWidthIndex;		// Index pointant dans scales
-	Vector3 currentScale;	// Dimension de la porte dans la scène
+	private int _scalesNumber=30;
+	private float _scalesMin=0.3f;
+	private float _scalesMax=1.2f;
+	private List<float> _scales = new List<float>();
+	private int _doorWidthIndex;		// Index pointant dans scales
+	private Vector3 _currentScale;	// Dimension de la porte dans la scène
 
 	// Nombre de réponses jouées (affichées dans la scène)
-	int nbAnswers=0;
+	private int _nbAnswers=0;
 
 	// Liste où sont enregistrés les largeur de portes jouées (pour le fichier de résultats)
-	List<float> ordreOuverture = new List<float>();
+	private List<float> _ordreOuverture = new List<float>();
 
 	// Modèle sélectionné par le sujet
-	float[] modelSrcValues;
+	private float[] _modelSrcValues;
 	// Modèle sélectionné par le psychologue
-	float[] modelDstValues;
+	private float[] _modelDstValues;
 	// Ecart de morphologie
-	float[] differenceModels;
+	private float[] _differenceModels;
 
 	// Fichier de résultats
-	StreamWriter file;
-	string path;
-	string fileName;
-	TextAsset textXml;
-	XmlDocument xmlDoc;
-	XmlDocument xmlModel;
+	private StreamWriter _file;
+	private string _path;
+	private string _fileName;
+	private TextAsset _textXml;
+	private XmlDocument _xmlDoc;
+	private XmlDocument _xmlModel;
 
-	List<bool> answers = new List<bool>();
-	bool next = false;
-	bool stop;
+	private List<bool> _answers = new List<bool>();
+	private bool _next = false;
+	private bool _stop;
 
-	int sujet;
-	int session;
-	int condition;
+	private int _sujet;
+	private int _session;
+	private int _condition;
 
 	void Start () {
 
 		// Assignation de labels à la condition du test et au sujet du test
-		sujet = PlayerPrefs.GetInt ("Sujet", 0);
-		condition = PlayerPrefs.GetInt ("Condition");		
+		_sujet = PlayerPrefs.GetInt ("Sujet", 0);
+		_condition = PlayerPrefs.GetInt ("Condition");		
 
 		// Création du fichier de résultats
-		fileName = System.DateTime.Now.ToString ();
-		fileName = fileName.Replace ("/", "-");
-		fileName = fileName.Replace (":", "-");
+		_fileName = System.DateTime.Now.ToString ();
+		_fileName = _fileName.Replace ("/", "-");
+		_fileName = _fileName.Replace (":", "-");
 
 		// Initialisation du tableau conprenant la largeur des portes.
 		initScales ();
@@ -72,120 +74,115 @@ public class InitSceneDoors : MonoBehaviour {
 		// Debug.Log (PlayerPrefs.GetString ("Model"));
 
 		// Recupération du nom des modèles dans modelName
-		string[] modelName = PlayerPrefs.GetString ("Model").Split (';');	
-		modelSrcValues = ReadModelsValue (modelName [0].Split ('/') [1]);	// Les modèles du sujet sont enregistrés en [0]
-		modelDstValues = ReadModelsValue (modelName [1].Split ('/') [1]);	// Les modèles du phychologue sont enregistrés en [1]
-		differenceModels = calculEcart (modelSrcValues, modelDstValues);
+		string[] modelName = PlayerPrefs.GetString ("Model").Split (';');
+		if(!modelName[0].Equals("")) {
+		_modelSrcValues = ReadModelsValue (modelName [0].Split ('/') [1]);	// Les modèles du sujet sont enregistrés en [0]
+		_modelDstValues = ReadModelsValue (modelName [1].Split ('/') [1]);	// Les modèles du phychologue sont enregistrés en [1]
+		_differenceModels = calculEcart (_modelSrcValues, _modelDstValues);
+		}
 
 		// Mise à jour de la largeur de porte.
 		updateWidthDoor ();
 
 		// Ecriture de la largeur de porte 
-		file = new StreamWriter ("Resultat.txt",true);
+		_file = new StreamWriter ("Resultat.txt",true);
 
 		// Utile pour la fonction update
-		stop = false;
+		_stop = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(!stop){
+		if(!_stop){
 
 			if (Input.GetMouseButtonDown (0) || Input.GetKeyDown(KeyCode.O)) {
 				Reponse(true);
-				next = true;
+				_next = true;
 			} else if (Input.GetMouseButtonDown (1) || Input.GetKeyDown(KeyCode.N)) {
 				Reponse(false);
-				next = true;
+				_next = true;
 			}
 
-			if(next){
-				if(scales.Count > 0){
+			if(_next){
+				if(_scales.Count > 0){
 					// Mise à jour de la largeur de porte.
 					updateWidthDoor ();
 				}
 				else {
-					stop=true;
+					_stop=true;
 					modifyXml ();
 					modifyTxT();
-					file.Close ();
+					_file.Close ();
 					Application.Quit();
 				}
-				next = false;
+				_next = false;
 			}
 		}
 	}
-
-	/**
-	 * 
-	 * 		Initialisation du tableau conprenant la largeur des portes.
-	 * 
-	 **/
+	/// <summary>
+	/// Initialisation du tableau conprenant la largeur des portes.
+	/// </summary>
 	void initScales(){
-		for (int i=0; i<scalesNumber; i++) {
-			scales.Add (Random.Range (scalesMin, scalesMax));
+		for (int i=0; i<_scalesNumber; i++) {
+			_scales.Add (Random.Range (_scalesMin, _scalesMax));
 		}
 	}
 
-	/**
-	 * 
-	 * 		Sélection aléatoire d'un index du tableau conprenant la largeur des portes.
-	 * 
-	 **/
+	/// <summary>
+	/// Sélection aléatoire d'un index du tableau conprenant la largeur des portes.
+	/// </summary>
+	/// <returns>The ramdom index.</returns>
 	int selectRamdomIndex(){
-		return Random.Range (0, scales.Count);
+		return Random.Range (0, _scales.Count);
 	}
 
-	/**
-	 * 
-	 * 		Mise à jour de la largeur de porte.
-	 * 
-	 **/
+	/// <summary>
+	/// Mise à jour de la largeur de porte.
+	/// </summary>
 	void updateWidthDoor(){
-
-		doorWidthIndex = selectRamdomIndex ();		// Récupération aléatoire d'un index
-		currentScale = piece.transform.localScale;	// Récupération de l'échelle de la porte dans la scène
-		currentScale.x = scales[doorWidthIndex];	// Affectation de la coordonée sélectionée dans scales[] sur currentScale.x (largeur de currentScale)
+		_doorWidthIndex = selectRamdomIndex ();		// Récupération aléatoire d'un index
+		_currentScale = _piece.transform.localScale;	// Récupération de l'échelle de la porte dans la scène
+		_currentScale.x = _scales[_doorWidthIndex];	// Affectation de la coordonée sélectionée dans scales[] sur currentScale.x (largeur de currentScale)
 		
 		// Ajout de la largeur dans ordreOuverture (pour le fichier de résultats) et suppression de la porte sélectionée dans scales
-		ordreOuverture.Add (currentScale.x);
-		scales.RemoveAt(doorWidthIndex);
+		_ordreOuverture.Add (_currentScale.x);
+		_scales.RemoveAt(_doorWidthIndex);
 		
 		// Modification de la largeur de porte dans la scène.
-		piece.transform.localScale = currentScale;	
+		_piece.transform.localScale = _currentScale;	
 		
 		// Ecriture de l'avancement dans la scène
-		nbAnswers++;
-		text.GetComponent<Text>().text = nbAnswers.ToString()+"/"+scalesNumber.ToString();
+		_nbAnswers++;
+		_text.GetComponent<Text>().text = _nbAnswers.ToString() + "/" + _scalesNumber.ToString();
 	}
 
 	void loadXMLFromAssest(){
 		
-		xmlModel = new XmlDocument();
-		textXml = (TextAsset)Resources.Load("Models", typeof(TextAsset));
-		xmlModel.LoadXml(textXml.text);
+		_xmlModel = new XmlDocument();
+		_textXml = (TextAsset)Resources.Load("Models", typeof(TextAsset));
+		_xmlModel.LoadXml(_textXml.text);
 	}
 
 	void modifyTxT(){
 		int essai = 1;
-		string modelSrcvalue = modelSrcValues [0].ToString () + "\t" + modelSrcValues [1].ToString () + "\t" + modelSrcValues [2].ToString ();
-		string modelDstvalue = modelDstValues [0].ToString () + "\t" + modelDstValues [1].ToString () + "\t" + modelDstValues [2].ToString ();
-		string modelDiffvalue = differenceModels [0].ToString () + "\t" + differenceModels [1].ToString () + "\t" + differenceModels [2].ToString ();
-		foreach (bool answer in answers) {
+		string modelSrcvalue = _modelSrcValues [0].ToString () + "\t" + _modelSrcValues [1].ToString () + "\t" + _modelSrcValues [2].ToString ();
+		string modelDstvalue = _modelDstValues [0].ToString () + "\t" + _modelDstValues [1].ToString () + "\t" + _modelDstValues [2].ToString ();
+		string modelDiffvalue = _differenceModels [0].ToString () + "\t" + _differenceModels [1].ToString () + "\t" + _differenceModels [2].ToString ();
+		foreach (bool answer in _answers) {
 			if (answer) {
-				file.WriteLine(sujet.ToString()+"\t"+condition.ToString()+"\t"+essai.ToString()+"\t"+ordreOuverture[essai-1].ToString()+"\t"+"1"+"\t"+modelSrcvalue+"\t"+modelDstvalue+"\t"+modelDiffvalue);
+				_file.WriteLine(_sujet.ToString() + "\t" + _condition.ToString() + "\t" + essai.ToString() + "\t" + _ordreOuverture[essai-1].ToString() + "\t" + "1" + "\t" + modelSrcvalue + "\t" + modelDstvalue + "\t" + modelDiffvalue);
 			} else {
-				file.WriteLine(sujet.ToString()+"\t"+condition.ToString()+"\t"+essai.ToString()+"\t"+ordreOuverture[essai-1].ToString()+"\t"+"0"+"\t"+modelSrcvalue+"\t"+modelDstvalue+"\t"+modelDiffvalue);
+				_file.WriteLine(_sujet.ToString() + "\t" + _condition.ToString() + "\t" + essai.ToString() + "\t" + _ordreOuverture[essai-1].ToString() + "\t" + "0" + "\t" + modelSrcvalue + "\t" + modelDstvalue + "\t" + modelDiffvalue);
 			}
 			essai++;
 		}
 	}
 	void modifyXml(){
 		int indexlist = 0;
-		XmlNode nodeact = xmlDoc.SelectSingleNode("Ouvertures");
+		XmlNode nodeact = _xmlDoc.SelectSingleNode("Ouvertures");
 		foreach(XmlElement node in nodeact.SelectNodes("Ouverture")){
-			node.FirstChild.InnerText = ordreOuverture[indexlist].ToString();
-			if(answers[indexlist]){
+			node.FirstChild.InnerText = _ordreOuverture[indexlist].ToString();
+			if(_answers[indexlist]){
 				node.LastChild.InnerText = "Oui";
 			}else{
 				node.LastChild.InnerText = "Non";
@@ -193,28 +190,28 @@ public class InitSceneDoors : MonoBehaviour {
 			indexlist++;
 		}
 
-		xmlDoc.Save(fileName+".xml");
+		_xmlDoc.Save(_fileName+".xml");
 	}
 
 	public void Reponse(bool rep){
-		answers.Add (rep);
-		next = true;
+		_answers.Add (rep);
+		_next = true;
 	}
 
 	void createXML(){
-		xmlDoc = new XmlDocument ();
-		XmlElement root = (XmlElement)xmlDoc.AppendChild(xmlDoc.CreateElement("Ouvertures"));
+		_xmlDoc = new XmlDocument ();
+		XmlElement root = (XmlElement)_xmlDoc.AppendChild(_xmlDoc.CreateElement("Ouvertures"));
 
-		for(int i = 0; i < scales.Count; i++){
-			XmlElement el = (XmlElement)root.AppendChild(xmlDoc.CreateElement("Ouverture"));
-			el.AppendChild(xmlDoc.CreateElement("Taille"));
-			el.AppendChild(xmlDoc.CreateElement("Reponse"));
+		for(int i = 0; i < _scales.Count; i++){
+			XmlElement el = (XmlElement)root.AppendChild(_xmlDoc.CreateElement("Ouverture"));
+			el.AppendChild(_xmlDoc.CreateElement("Taille"));
+			el.AppendChild(_xmlDoc.CreateElement("Reponse"));
 		}
 	}
 
 	float[] ReadModelsValue(string name){
 		string nameNode= "", waist = "", hips= "", chest= "";
-		foreach (XmlElement node in xmlModel.SelectNodes("Models/Model")) {
+		foreach (XmlElement node in _xmlModel.SelectNodes("Models/Model")) {
 			nameNode = node.GetAttribute ("name");
 			if(nameNode.Equals(name)){
 				XmlNode waistNode = node.SelectSingleNode("Waist");
@@ -238,5 +235,22 @@ public class InitSceneDoors : MonoBehaviour {
 		resultat [1] = (dst [1] - src [1]) / src [1] * 100;
 		resultat [2] = (dst [2] - src [2]) / src [2] * 100;
 		return resultat;
+	}
+
+	public GameObject piece {
+		get {
+			return _piece;
+		}
+		set { 
+			_piece = value;
+		}
+	}
+	public GameObject text{
+		get {
+			return _text;
+		}
+		set { 
+			_text = value;
+		}
 	}
 }
