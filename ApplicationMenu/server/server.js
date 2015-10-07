d9ebd55c9aa18b614a8d11818598e15e6b5985be
raydeurs <briand.jp35@gmail.com>
2015-10-07 14:54:07 +0200

    modification du server node.js

--- ApplicationMenu/server/server.js
+++ ApplicationMenu/server/server.js
@@ -1,39 +1,20 @@
 var net = require('net');
 var http = require('http');
+var express = require('express')();
 
 const HTTP_PORT = '3000'; 
+const UNITY_PORT = '8000'; 
 
-/*
-http.listen(3000, function() {});
-server.listen(8124, function() {
 
+express.get('/', function(req, res){
+  res.sendFile('index.html', { root : __dirname});
 });
 
-app.get('/', function(req, res){
-  res.send('Bonjour');
-});
+express.listen(HTTP_PORT);
 
-var server = net.createServer(function(c) {
-  console.log('client connected');
-    
-    app.get('/test', function(req, res){
-        c.write('Requete GET test \r\n');
-        c.pipe(c);
+net.createServer(function (socket) {    
+
+    express.get('/baton:id', function(req, res){
+        console.log(req.params.id);
     });
-});
-*/
-
-function handleRequest(request, response){
-    
-    response.end('It Works!! Path Hit: ' + request.url);
-    // Envoi de la page HTML
-}
-
-//Create a server
-var server = http.createServer(handleRequest);
-
-//Lets start our server
-server.listen(HTTP_PORT, function(){
-    //Callback triggered when server is successfully listening. Hurray!
-    console.log("Server listening on: http://localhost:%s", HTTP_PORT);
-});
+}).listen(UNITY_PORT);
