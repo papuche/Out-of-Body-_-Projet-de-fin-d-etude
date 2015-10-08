f1020e113b213233740c5fe064e567fa6a5085ae
raydeurs <briand.jp35@gmail.com>
2015-10-08 12:46:33 +0200

    Correction bug connexion/deconnexion socket c#

--- ApplicationMenu/server/server.js
+++ ApplicationMenu/server/server.js
@@ -1,31 +1,39 @@
-var net = require('net');
-var http = require('http');
-var express = require('express')();
+var net = require('net'); // Librairie de socket c#.
+var express = require('express')(); // Librairie pour la gestion des requètes http.
 
-const HTTP_PORT = '3000'; 
-const UNITY_PORT = '8000'; 
+const HTTP_PORT = '3000';  // port http pour le navigateur client.
+const UNITY_PORT = '8000';  // port socket unity.
 
+// tableau de sockets C# connectées.
+var clients_unity = [];
 
+// On écoute les requète http sur le port HTTP_PORT
+express.listen(HTTP_PORT);
+
+// page par défaut de l'application (à revoir avec Sarah et Florian).
 express.get('/', function(req, res){
   res.sendFile('index.html', { root : __dirname});
 });
 
-express.listen(HTTP_PORT);
+// Evenement lors de la connexion d'une socket c#.
+net.createServer(function (socket) {    
+    
+    // on ajoute la socket au tableau. 
+    clients_unity.push(socket);    
 
-var server = net.createServer(function (socket) {    
-
+    // Url à revoir avec Sarah et Florian.
     express.get('/baton:id', function(req, res){
         var id = req.params.id;
-        var ret = socket.write('baton' + req.params.id);
-        console.log(ret);
-        res.send(id);
+        // Ecriture sur le client[0] => socket couante.
+        clients_unity[0].write('baton' + req.params.id);    
     });
-    
-    socket.on('end', function() {
-        server.close();
-        express
-    });
-    
-});
 
-server.listen(UNITY_PORT);
+    // Evenement lors de la déconnexion de la socket c#.
+    socket.on('close', function () {
+        // Suppression de la socket du tableau de clients.
+        clients_unity.splice(clients_unity.indexOf(socket), 1);
+    });
+    
+    socket.on('error', function(e){});
+    
+}).listen(UNITY_PORT); // le serveur écoute sur le port UNITY_PORT pour les sockets c#.
