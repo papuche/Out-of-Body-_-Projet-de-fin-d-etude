af577db7c7a207532172238520ed89d00b2cbf6f
Julien <j0lemais@enib.fr>
2015-10-14 17:21:43 +0200

    Correction des bugs liés au contrôle du baton.

--- ExperienceSortieCorps/Assets/AssetsPerso/Scripts/MouvementHydra.cs
+++ ExperienceSortieCorps/Assets/AssetsPerso/Scripts/MouvementHydra.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class MouvementHydra : MonoBehaviour {
-
+	
 	public bool 				ActivateRotations;
 	public SixenseHands			Hand;
 	public Vector3				Sensitivity = new Vector3( 0.01f, 0.01f, 0.01f );
@@ -11,7 +11,7 @@ public class MouvementHydra : MonoBehaviour {
 	protected Quaternion		m_initialRotation;
 	protected Vector3			m_initialPosition;
 	protected Vector3			m_baseControllerPosition;
-
+	
 	// Use this for initialization
 	void Start () {
 		m_initialRotation = this.gameObject.transform.localRotation;
@@ -31,7 +31,7 @@ public class MouvementHydra : MonoBehaviour {
 			UpdateObject(controller);
 		}
 	}
-
+	
 	/*void OnGUI()
 	{
 		if ( !m_enabled )
@@ -47,53 +47,53 @@ public class MouvementHydra : MonoBehaviour {
 		{
 			// enable position and orientation control
 			m_enabled = !m_enabled;
-
+			
 			// delta controller position is relative to this point
-			m_baseControllerPosition = new Vector3( -controller.Position.z * Sensitivity.x,
+			m_baseControllerPosition = new Vector3( controller.Position.x * Sensitivity.x,
 			                                       controller.Position.y * Sensitivity.y,
-			                                       controller.Position.x * Sensitivity.z );
+			                                       controller.Position.z * Sensitivity.z );
 			
 			// this is the new start position
 			m_initialPosition = this.gameObject.transform.localPosition;
-
+			
 			gameObject.GetComponentInChildren<MeshRenderer>().enabled = m_enabled;
 		}
-
+		
 		/*if (m_enabled && controller.GetButtonDown (SixenseButtons.JOYSTICK)) 
 		{
 			this.gameObject.transform.Translate(0.0f,controller.JoystickY,controller.JoystickX);
 		}*/
 		if ( m_enabled )
 		{
-			//Déplacement du gameObject au joystick
+			///Déplacement du gameObject au joystick
 			m_initialPosition += new Vector3(0.0f,controller.JoystickY*0.01f,controller.JoystickX*0.01f);
 			Debug.Log(new Vector3(0.0f,controller.JoystickY,controller.JoystickX));
-
+			
 			//Déplacement en profondeur du gameObject via RB RT
 			if(controller.GetButton( SixenseButtons.BUMPER )){
-			   m_initialPosition += new Vector3(0.005f,0.0f,0.0f);
+				m_initialPosition += new Vector3(0.005f,0.0f,0.0f);
 			}else if (controller.GetButton( SixenseButtons.TRIGGER )){
 				m_initialPosition -= new Vector3(0.005f,0.0f,0.0f);
 			}
-
+			
 			// Rotation suivant l'axe Y via les boutons 1 & 3
 			if(controller.GetButton( SixenseButtons.ONE )){
 				m_initialRotation.y += 0.005f;
 			}else if (controller.GetButton( SixenseButtons.THREE )){
 				m_initialRotation.y -= 0.005f;
 			}
-
+			
 			// Rotation suivant l'axe Z via les boutons 2 & 4
-			/*if(controller.GetButton( SixenseButtons.TWO )){
+			if(controller.GetButton( SixenseButtons.TWO )){
 				m_initialRotation.z += 0.005f;
 			}else if (controller.GetButton( SixenseButtons.FOUR )){
 				m_initialRotation.z -= 0.005f;
-			}*/
-
+			}
+			
 			// Modifie la sensibilité via les boutons 2 & 4
 			if(controller.GetButton( SixenseButtons.TWO )){
 				Sensitivity += new Vector3(0.00001f,0.00001f,0.00001f);
-					/*m_baseControllerPosition = new Vector3( -controller.Position.z * Sensitivity.x,
+				/*m_baseControllerPosition = new Vector3( -controller.Position.z * Sensitivity.x,
 				                                      	 controller.Position.y * Sensitivity.y,
 				                                      	 controller.Position.x * Sensitivity.z );*/
 			}else if (controller.GetButton( SixenseButtons.FOUR )){
@@ -104,20 +104,20 @@ public class MouvementHydra : MonoBehaviour {
 				                                       	controller.Position.x * Sensitivity.z );*/
 				}
 			}
-
+			
 			UpdatePosition( controller );
-
+			
 			if (ActivateRotations){
 				UpdateRotation( controller );
 			}
 		}
 	}
-
+	 
 	protected void UpdatePosition( SixenseInput.Controller controller )
 	{
-		Vector3 controllerPosition = new Vector3( -controller.Position.z * Sensitivity.x,
+		Vector3 controllerPosition = new Vector3( controller.Position.x * Sensitivity.x,
 		                                         controller.Position.y * Sensitivity.y,
-		                                         controller.Position.x * Sensitivity.z );
+		                                         controller.Position.z * Sensitivity.z );
 		
 		// distance controller has moved since enabling positional control
 		Vector3 vDeltaControllerPos = controllerPosition - m_baseControllerPosition;
@@ -129,11 +129,11 @@ public class MouvementHydra : MonoBehaviour {
 	
 	protected void UpdateRotation( SixenseInput.Controller controller )
 	{
-		Quaternion controllerRotation = new Quaternion( -controller.Rotation.z,
-		                                         controller.Rotation.y,
-		                                         controller.Rotation.x,
-		                                         controller.Rotation.w);
-
+		Quaternion controllerRotation = new Quaternion( controller.Rotation.x,
+		                                               controller.Rotation.y,
+		                                               controller.Rotation.z,
+		                                               controller.Rotation.w);
+		
 		this.gameObject.transform.localRotation = controllerRotation * m_initialRotation;
 		//controller.Rotation.z
 	}
