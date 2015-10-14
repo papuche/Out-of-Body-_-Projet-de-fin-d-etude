bcf2b6739f7536bdd09097ec37a49538cb12bce3
raydeurs <briand.jp35@gmail.com>
2015-10-14 15:05:44 +0200

    suppression fichier index.html + ajout des url GET sur server.js

--- ApplicationMenu/server/server.js
+++ ApplicationMenu/server/server.js
@@ -7,6 +7,7 @@ const HTTP_PORT = '3000';
 const UNITY_PORT = '8000'; 
 
 var app = express();
+
 app.get('/', function(req, res){
     if(req=="portes")
         res.sendFile(path.join(__dirname,'../client/templates','/portes.html'));
@@ -15,24 +16,41 @@ app.get('/', function(req, res){
 });
 
 app.use(express.static(__dirname+"/.."));
-
 app.listen(HTTP_PORT);
 
 var server = net.createServer(function (socket) {    
 
-    express.get('/baton:id', function(req, res){
-        var id = req.params.id;
-        var ret = socket.write('baton' + req.params.id);
-        console.log(ret);
-        res.send(id);
-    });
+    getAndSendWithoutParams(socket, 'baton');
+    getAndSendWithoutParams(socket, 'morphing');
+    getAndSendWithoutParams(socket, 'baton_morphing');
+    getAndSendWithoutParams(socket, 'stop');
+    getAndSendWithoutParams(socket, 'nothing');
     
+    getAndSendWithParams(socket, 'e');
+    getAndSendWithParams(socket, 'db');
+    getAndSendWithParams(socket, 'dh');
+
     socket.on('end', function() {
         server.close();
-        express
     });
+    
+    socket.on('error', function() {});
     
 });
 
-
 server.listen(UNITY_PORT);
+
+
+function getAndSendWithoutParams(socket, url) {
+    app.get('/' + url, function(req, res) {
+          socket.write(url);
+    });
+}
+
+function getAndSendWithParams(socket, url) {
+    url += ":values";
+    app.get('/' + url, function(req, res) {
+        var send = req.originalUrl.replace('/', '');
+        socket.write(url);
+    });
+}
