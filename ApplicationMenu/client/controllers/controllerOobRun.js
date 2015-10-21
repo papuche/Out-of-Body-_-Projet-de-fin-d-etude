348a3e9373a85be7ec72c46e669c9d655ed7837c
PestoP <f1zanard@enib.fr>
2015-10-21 18:08:07 +0200

    retour clavier enlevé à la derniere étape + checkbox ghost

--- ApplicationMenu/client/controllers/controllerOobRun.js
+++ ApplicationMenu/client/controllers/controllerOobRun.js
@@ -1,5 +1,15 @@
-menu.controller('OobRunCtrl', function ($scope, $state, $http, $rootScope) {
+menu.controller('OobRunCtrl', function ($scope, $state, $http, $rootScope, $document) {
+	// enlever le retour de backspace key
+	$document.on('keydown', function(e){
+          if(e.which === 8 && e.target.nodeName !== "INPUT" || e.target.nodeName !== "SELECT"){ // you can add others here.
+              e.preventDefault();
+          }
+ 	 });	
+
 	$rootScope.chemin = 'Accueil > Sortie de corps > Application en cours';
+	$scope.ghost = function () {
+		$http.get('/ghost');
+	}
 
 	$scope.exit = function () {
 		$http.get('/stop');
--- ApplicationMenu/client/controllers/controllerPortesRun.js
+++ ApplicationMenu/client/controllers/controllerPortesRun.js
@@ -1,4 +1,11 @@
-menu.controller('PortesRunCtrl', function ($scope,$state,$http, $rootScope) {
+menu.controller('PortesRunCtrl', function ($scope,$state,$http, $rootScope, $document) {
+		// enlever le retour de backspace key
+	$document.on('keydown', function(e){
+          if(e.which === 8 && e.target.nodeName !== "INPUT" || e.target.nodeName !== "SELECT"){ // you can add others here.
+              e.preventDefault();
+          }
+ 	 });	
+	
 	$rootScope.chemin = 'Accueil > Exercice des portes > Application en cours';
 
 	$scope.end_click = function () {
