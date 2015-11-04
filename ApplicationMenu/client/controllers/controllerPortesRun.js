32e76decf6de238383241f8cfef734d7e6ee23b8
PestoP <f1zanard@enib.fr>
2015-11-04 13:57:51 +0100

    mise en place d'un pool pour porte

--- ApplicationMenu/client/controllers/controllerPortesRun.js
+++ ApplicationMenu/client/controllers/controllerPortesRun.js
@@ -1,11 +1,22 @@
-menu.controller('PortesRunCtrl', function ($scope,$state,$http, $rootScope, $document) {
+menu.controller('PortesRunCtrl', function ($scope,$state,$http, $rootScope, $document, $timeout) {
+	var pool = function () {
+		if($state.current.name !== 'runPortes')
+			return;
+		$http.get('/porte').then(function(resp){
+			console.log(resp);
+			$state.go('mainMenu');
+			return;
+		});
+		$timeout(pool, 200);
+
+	}
 		// enlever le retour de backspace key
 	$document.on('keydown', function(e){
           if(e.which === 8){
               e.preventDefault();
           }
  	 });	
-	
+	pool();
 	$rootScope.chemin = 'Accueil > Exercice des portes > Application en cours';
 
 	$scope.end_click = function () {
