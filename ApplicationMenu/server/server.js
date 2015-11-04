028ec701f5a45bca02b0fccb8034a7590df7cce5
raydeurs <briand.jp35@gmail.com>
2015-11-04 10:27:08 +0100

    maj server

--- ApplicationMenu/server/server.js
+++ ApplicationMenu/server/server.js
@@ -3,11 +3,13 @@ var http = require('http');
 var express = require('express');
 var path = require('path');
 
-const HTTP_PORT = '3000'; 
-const UNITY_PORT = '8000'; 
+const HTTP_PORT = '3000';
+const UNITY_PORT = '8000';
 
 var app = express();
 var clients_unity = [];
+
+var dors_finish = false;
 
 app.get('/', function(req, res){
     if(req=="portes")
@@ -19,10 +21,44 @@ app.get('/', function(req, res){
 app.use(express.static(__dirname+"/.."));
 app.listen(HTTP_PORT);
 
+var server = net.createServer(function (socket) {
+    clients_unity.push(socket);
+
+    socket.on('close', function(e) {
+        clients_unity.splice(clients_unity.indexOf(socket), 1);
+    });
+
+    socket.on('data', function(data) {
+      console.log(data);
+    });
+
+    process.on('uncaughtException', function (err) {});
+
+    getAndSendWithoutParams(socket, 'avatar');
+    getAndSendWithoutParams(socket, 'exit');
+    getAndSendWithoutParams(socket, 'baton');
+    getAndSendWithoutParams(socket, 'morphing');
+    getAndSendWithoutParams(socket, 'baton_morphing');
+	  getAndSendWithoutParams(socket, 'ghost');
+    getAndSendWithoutParams(socket, 'stop');
+    getAndSendWithoutParams(socket, 'nothing');
+    getAndSendWithoutParams(socket, 'M_avatar');
+    getAndSendWithoutParams(socket, 'F_avatar');
+    getAndSendDorsFinish(socket);
+
+    getAndSendWithParams(socket, 'e');
+    getAndSendWithParams(socket, 'db');
+    getAndSendWithParams(socket, 'dh');
+    getAndSendWithParams(socket, 'validerAvatar');
+});
+server.listen(UNITY_PORT);
+
+function getAndSendDorsFinish(socket) {
+    getAndSendWithoutParams(socket, 'porte')
+};
 
 function getAndSendWithoutParams(socket, url) {
     app.get('/' + url, function(req, res) {
-        console.log(url);
         clients_unity[0].write(url);
         res.end();
     });
@@ -32,40 +68,7 @@ function getAndSendWithParams(socket, url) {
     url += "/:values";
     app.get('/' + url, function(req, res) {
         var send = req.originalUrl.replace('/', '');
-        console.log(send);
         clients_unity[0].write(send);
         res.end();
     });
 }
-
-var server = net.createServer(function (socket) {    
-    
-    clients_unity.push(socket);
-      
-    socket.on('close', function(e) { 
-        clients_unity.splice(clients_unity.indexOf(socket), 1);
-    });
-
-    
-    process.on('uncaughtException', function (err) {
-        console.error(err.stack);
-    });
-    
-    getAndSendWithoutParams(socket, 'avatar');
-    getAndSendWithoutParams(socket, 'baton');
-    getAndSendWithoutParams(socket, 'morphing');
-    getAndSendWithoutParams(socket, 'baton_morphing');
-	getAndSendWithoutParams(socket, 'ghost');
-    getAndSendWithoutParams(socket, 'stop');
-    getAndSendWithoutParams(socket, 'nothing');
-    getAndSendWithoutParams(socket, 'M_avatar');
-    getAndSendWithoutParams(socket, 'F_avatar');
-    
-    getAndSendWithParams(socket, 'e');
-    getAndSendWithParams(socket, 'db');
-    getAndSendWithParams(socket, 'dh');
-    getAndSendWithParams(socket, 'validerAvatar');
-});
-
-server.timeout = 0;
-server.listen(UNITY_PORT);
